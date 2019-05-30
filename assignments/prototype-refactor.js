8bcd323e3066a24f81e1c9106d7dde1af36aae10
eric <Eric355402@yahoo.com>
2019-05-29 21:45:50 -0600

    mvp complete

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -7,81 +7,38 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
-/*
-  Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
 
-  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
-
-  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
-  
-  Each constructor function has unique properties and methods that are defined in their block comments below:
-*/
-//constructors vv
-function GameObject(attrs) {
-  this.createdAt = attrs.createdAt;
-  this.name = attrs.name;
-  this.dimensions = attrs.dimensions;
+class GameObject {
+  constructor(attrs) {
+    this.createdAt = attrs.createdAt;
+    this.name = attrs.name;
+    this.dimensions = attrs.dimensions;
+  }
+  destroy() {
+    return `${this.name} was removed from the game`;
+  }
 }
 
-function CharacterStats(stats) {
-  GameObject.call(this, stats);
-  this.healthPoints = stats.healthPoints;
+class CharacterStats extends GameObject {
+  constructor(attrs) {
+    super(attrs);
+    this.healthPoints = attrs.healthPoints;
+  }
+  takeDamage() {
+    return `${this.name} took damage`;
+  }
 }
-
-function Humanoid(noid) {
-  CharacterStats.call(this, noid);
-  this.team = noid.team;
-  this.weapons = noid.weapons;
-  this.language = noid.language;
+class Humanoid extends CharacterStats {
+  constructor(attrs) {
+    super(attrs);
+    this.team = attrs.team;
+    this.weapons = attrs.weapons;
+    this.language = attrs.language;
+  }
+  greet() {
+    return `${this.name} offers a greeting in ${this.language}`;
+  }
 }
-//Prototypes vv
-GameObject.prototype.destroy = function() {
-  return `${this.name} was removed from the game`;
-};
-
-CharacterStats.prototype = Object.create(GameObject.prototype);
-CharacterStats.prototype.takeDamage = function() {
-  return `${this.name} took damage`;
-};
-
-Humanoid.prototype = Object.create(CharacterStats.prototype);
-Humanoid.prototype.greet = function() {
-  return `${this.name} offers a greeting in ${this.language}`;
-};
-//reassigning prototypes
-
-/*
-  === GameObject ===
-  * x createdAt
-  * x name
-  * x dimensions (These represent the character's size in the video game)
-  * x destroy() // prototype method that returns: `${this.name} was removed from the game.`
-*/
-
-/*
-  === CharacterStats ===
-  * x healthPoints
-  * x takeDamage() // prototype method -> returns the string '<object name> took damage.'
-  * should inherit destroy() from GameObject's prototype
-*/
-
-/*
-  === Humanoid (Having an appearance or character resembling that of a human.) ===
-  * team
-  * weapons
-  * language
-  * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
-  * should inherit destroy() from GameObject through CharacterStats
-  * should inherit takeDamage() from CharacterStats
-*/
-
-/*
- * Inheritance chain: GameObject -> CharacterStats -> Humanoid
- * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
- * Instances of CharacterStats should have all of the same properties as GameObject.
- */
-
-// Test you work by un-commenting these 3 objects and the list of console logs below:
 
 const mage = new Humanoid({
   createdAt: new Date(),
